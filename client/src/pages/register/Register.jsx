a3393aa317c48fe96d16911c0ad0dc40a34690d9
Hannah Nguyen <myhanh.nguyen.969317598@gmail.com>
2024-03-24 17:27:21 +0200

    create Register page

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,7 +1,81 @@
-import React from 'react';
+import axios from 'axios';
+import { useRef } from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './register.scss';
 
-const Register = () => {
-  return <div>register</div>;
-};
+export default function Register() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const navigate = useNavigate();
 
-export default Register;
+  const emailRef = useRef();
+
+  const handleStart = () => {
+    setEmail(emailRef.current.value);
+  };
+
+  const handleFinish = async (e) => {
+    e.preventDefault();
+    try {
+      console.log({ email, username, password });
+      await axios.post('http://localhost:8080/api/auth/register', {
+        email,
+        username,
+        password,
+      });
+      navigate('/login');
+    } catch (err) {
+      console.log({ errorClient: err });
+    }
+  };
+
+  return (
+    <div className="register">
+      <div className="top">
+        <div className="wrapper">
+          <img
+            className="logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt=""
+          />
+          <button className="loginButton">Sign In</button>
+        </div>
+      </div>
+      <div className="container">
+        <h1>Unlimited movies, TV shows, and more.</h1>
+        <h2>Watch anywhere. Cancel anytime.</h2>
+        <p>
+          Ready to watch? Enter your email to create or restart your membership.
+        </p>
+        {!email ? (
+          <div className="input">
+            <input type="email" placeholder="email address" ref={emailRef} />
+            <button className="registerButton" onClick={handleStart}>
+              Get Started
+            </button>
+          </div>
+        ) : (
+          <form className="input">
+            <input
+              type="username"
+              placeholder="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button className="registerButton" onClick={handleFinish}>
+              Start
+            </button>
+          </form>
+        )}
+      </div>
+    </div>
+  );
+}
